ea436b4d85d40bb5746d9dd6c4df309d822e9023
Quatton <atomicatives@gmail.com>
2022-09-26 20:13:44 +0900

    a bit buggy

---
+++ src/components/Header.svelte
@@ -0,0 +1,48 @@
+<template>
+  <header
+    class="fixed w-full h-16 flex items-center px-4 backdrop-blur-lg shadow-md"
+  >
+    <a href="/">
+      <h2 class="quatton text-gradient">Quattonary</h2>
+    </a>
+  </header>
+</template>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Kumar+One+Outline&family=M+PLUS+Rounded+1c:wght@100;300;400;500;700&family=Mitr:wght@200;300;400;500&family=Ms+Madi&display=swap");
+
+  :root {
+    --astro-gradient: linear-gradient(
+      -90deg,
+      var(--color-sea),
+      var(--color-sun)
+    );
+  }
+
+  .quatton {
+    font-family: "Ms Madi", serif;
+    font-weight: 600;
+    font-size: 2rem;
+  }
+
+  .text-gradient {
+    font-weight: 900;
+    background-image: var(--astro-gradient);
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-size: 200% 100%;
+    background-position-x: 100%;
+    border-radius: 0.4rem;
+    animation: pulse 4s cubic-bezier(0.76, 0.24, 0.24, 0.76) infinite;
+  }
+
+  @keyframes pulse {
+    0%,
+    100% {
+      background-position-x: 0%;
+    }
+    50% {
+      background-position-x: 80%;
+    }
+  }
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,33 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      sea: {
+        DEFAULT: "#65DEF1",
+        light: "#A8DCD1",
+      },
+      sand: "#DCE2C8",
+      sun: {
+        DEFAULT: "#F96900",
+        light: "#F17F29",
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none",
+        },
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+      });
+    }),
+  ],
+};
