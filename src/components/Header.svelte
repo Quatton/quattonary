36ce995a51956e5b815578d02a396f246b942e00
Quatton <atomicatives@gmail.com>
2022-09-29 20:49:48 +0900

    working index

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,22 +1,17 @@
 <template>
-  <header class="w-full h-16 flex items-center px-4 bg-slate-900">
-    <a href="/">
-      <h2 class="quatton">Quattonary</h2>
-    </a>
+  <header class="w-full flex items-center px-4 py-4">
+    <a href="/"> Quattonary </a>
   </header>
 </template>
 
 <style>
-  :root {
-    --astro-gradient: linear-gradient(
-      -90deg,
-      var(--color-sea),
-      var(--color-sun)
-    );
+  header {
+    background-color: var(--color-bg);
   }
 
-  .quatton {
-    font-family: "Lato", sans-serif;
-    font-weight: 600;
+  header > a {
+    font-size: 1.5em;
+    font-family: "Merriweather", serif;
+    font-weight: 700;
   }
 </style>
