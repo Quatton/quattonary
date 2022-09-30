4b16767ee64454cf12fc2a0e80ab3d1b5ce3c700
Quatton <atomicatives@gmail.com>
2022-09-30 17:14:18 +0900

    libravue final fix

--- src/components/libravue/Libravue.vue
+++ src/components/libravue/Libravue.vue
@@ -1,6 +1,8 @@
 <template>
-  <article>
-    <h1>A library where every book is written by one author.</h1>
-    <h2>Coming soon...</h2>
-  </article>
+  <section>
+    <article>
+      <h1>A library where every book is written by one author.</h1>
+      <h2>Coming soon...</h2>
+    </article>
+  </section>
 </template>
