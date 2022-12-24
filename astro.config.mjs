4cdf79e089db51da610e6e8edfd8e450ecdf5df9
Quatton <atomicatives@gmail.com>
2022-12-25 00:26:49 +0900

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,19 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
+import vue from "@astrojs/vuse";
+import vercel from "@astrojs/vercel/serverless";
 
-import vue from "@astrojs/vue";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx(), vue()],
+  integrations: [tailwind(), svelte(), mdx(), vue(), image()],
   vite: {
     ssr: {
-      external: ["svgo"]
-    }
-  }
+      external: ["svgo"],
+    },
+  },
+  output: "server",
+  adapter: vercel(),
 });
