e8df27148580b4f1e33eb3c8cf428774beecdb65
Quatton <atomicatives@gmail.com>
2023-12-27 03:39:02 +0900

    added memoiz

--- src/utils/pages.ts
+++ src/utils/pages.ts
@@ -2,6 +2,7 @@ import { z } from "astro/zod";
 
 export const pageSchema = z.object({
   id: z.number(),
+  ids: z.array(z.number()).optional(),
   cover: z.string().optional(),
   path: z.string(),
   name: z.string(),
@@ -16,9 +17,3 @@ export const pages: z.infer<typeof pageSchema>[] = Array.from(
     name: (i + 1).toString(),
   })
 );
-for (const idx of [4, 8, 9, 13, 14, 15, 19]) {
-  pages[idx - 1] = {
-    ...pages[idx - 1],
-    path: "toosmall",
-  };
-}
