e3d7a99fb607c67e62168e09c6932e6eacd2c641
Nutchanon Taechasuk <58178159+Quatton@users.noreply.github.com>
2023-12-26 05:35:24 +0000

    unused

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 import vue from "@astrojs/vue";
 import vercel from "@astrojs/vercel/serverless";
+import icon from "astro-icon";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx(), vue(), image()],
+  integrations: [tailwind(), svelte(), mdx(), vue(), image(), icon()],
   vite: {
     ssr: {
       external: ["svgo"],
